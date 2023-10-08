1420f2ef9de5ff0d350868240cfcab46b88a0302
Александр <@DESKTOP-P44AI30>
2023-10-08 16:30:29 +0500

    create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h2>My To-Do List</h2>
+      <input [(ngModel)]="newTask" placeholder="New task" />
+      <button (click)="addTask()">Add</button>
+      <ul>
+        <li *ngFor="let task of tasks">{{ task }}</li>
+      </ul>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'todolist';
+  newTask = '';
+  tasks: string[] = [];
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
 }
